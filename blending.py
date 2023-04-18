ad32773c72aca03a7983d3430284185a0e9a9fd7
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-04-18 22:07:45 +0800

    Optimized split_channels() to list comprehension

--- blending.py
+++ blending.py
@@ -92,10 +92,11 @@ class MultiBandBlending(Blending):
     def split_channels(self, image: np.ndarray) -> typing.List[np.ndarray]:
         # SCORE +1: Split an image into multiple channels
         # Hint: (H, W, C) -> [(H, W), (H, W), ..., (H, W)]
-        channels = []
-        for i in range(image.shape[2]):
-            channels.append(image[:, :, i])
-        return channels  # Hint: Replace this line with the appropriate expression
+        # channels = []
+        # for i in range(image.shape[2]):
+        #     channels.append(image[:, :, i])
+        # return channels  # (Done) Hint: Replace this line with the appropriate expression
+        return [image[:, :, i] for i in range(image.shape[2])]  # List comprehension
 
     def join_channels(self, channels: typing.List[np.ndarray]) -> np.ndarray:
         # SCORE +1: Combine the split channels to a single image of shape (H, W, C)
