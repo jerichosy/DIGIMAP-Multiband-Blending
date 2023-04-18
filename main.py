f9bfda39c7ac9aa1226f9320511a7332465da2cd
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-04-19 03:37:40 +0800

    Add figure summarizing all inputs and outputs

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 
 import blending
 
+import matplotlib.pyplot as plt
+
 
 def read_image(path) -> np.ndarray:
     data = imageio.imread(path)
@@ -33,9 +35,33 @@ if __name__ == "__main__":
     mask = read_mask("mask.jpg")
 
     blender = blending.MultiBandBlending(num_levels=compute_num_levels(target))
-    composite = blender(target, source, mask)
-    write_image(composite, "multiband.jpg")
+    composite_multiband = blender(target, source, mask)
+    write_image(composite_multiband, "multiband.jpg")
 
     blender = blending.NaiveBlending()
-    composite = blender(target, source, mask)
-    write_image(composite, "naive.jpg")
+    composite_naive = blender(target, source, mask)
+    write_image(composite_naive, "naive.jpg")
+
+    plt.figure(figsize=(11, 7))
+    plt.subplot(2, 3, 1)
+    plt.imshow(target)
+    plt.title("Target")
+    plt.axis("off")
+    plt.subplot(2, 3, 2)
+    plt.imshow(source)
+    plt.title("Source")
+    plt.axis("off")
+    plt.subplot(2, 3, 3)
+    plt.imshow(mask, cmap="gray")
+    plt.title("Mask")
+    plt.axis("off")
+    plt.subplot(2, 3, 4)
+    plt.imshow(composite_multiband)
+    plt.title("Multiband Blending")
+    plt.axis("off")
+    plt.subplot(2, 3, 5)
+    plt.imshow(composite_naive)
+    plt.title("Naive Blending")
+    plt.axis("off")
+    plt.show()
+    
